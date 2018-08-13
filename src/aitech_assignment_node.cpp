13e2d1bf966f6d296eff0ade32399004c3028258
ahmedwahhab <ahmed.wahhab@gmail.com>
2018-08-13 10:19:56 +0800

    4th commit

--- src/aitech_assignment_node.cpp
+++ src/aitech_assignment_node.cpp
@@ -10,32 +10,27 @@
 #include "iomanip"
 
 
-/**std::vector<float> range2point(float ang_min,float ang_max, float inc,float range, int indx){
-
-std::vector<float>   pt;
-pt.push_back(0.1);
-pt.push_back(0.3);
-return pt;
-
-}*/
-
-void Callback(const sensor_msgs::LaserScan::ConstPtr& scan){
-         
-
-  geometry_msgs::Twist msg;
+class ParkingRobot
+{
+public:
   
 
-  int n, i;
-  float sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, average1, average2, average3;
-  const double FORWARD_SPEED_MPS = 0.5;
-  msg.linear.x = FORWARD_SPEED_MPS;
+  static void Callback(const sensor_msgs::LaserScan::ConstPtr& scan){
+         
 
+  double* new_readings = new double [720];
+
+  int n, i;
+  float sum1 = 0.0, sum2 = 0.0, sum3 = 0.0;
+  
   for(i = 0; i < 719; ++i)
     {
+        new_readings[i] = scan->ranges[i];
         
-        if(scan->ranges[i]>30){
-          scan->ranges[i]=0.0;
+        if(new_readings[i]>30){
+          new_readings[i]=0.0;
         }
+
     }
 
 
@@ -43,74 +38,81 @@ void Callback(const sensor_msgs::LaserScan::ConstPtr& scan){
   for(i = 156; i < 162; ++i)
     {
         
-        sum1 += scan->ranges[i];
+        sum1 += new_readings[i];
     }
 
-    average1 = sum1 / n;
-    printf("%.6f", average1);
+    avg1 = sum1 / 6;
+    
 
 
   //left leg averge pointcloud
   for(i = 557; i < 563; ++i)
     {
         
-        sum2 += scan->ranges[i];
+        sum2 += new_readings[i];
     }
 
-    average2 = sum2 / n;
-    printf("%.6f", average2);
+    avg2 = sum2 / 6;
+    
 
   //not seeing other legs average 
   for(i = 181; i < 539; ++i)
     {
         
-        sum3 += scan->ranges[i];
+        sum3 += new_readings[i];
     }
 
-    average3 = sum3 / n;
-    printf("%.6f", average3);
+    avg3 = sum3 / n;
+ 
 
-  
-
-  if((1.73<average1<1.77)&&(1.73<average2<1.77)&&(average3<0.5)){
-
-    msg.linear.x = 0;
-    
-  }
-  
-      //ROS_INFO("position=: [%f],[%f],[%f],[%f],[%f],[%f],[%f],[%f]",scan->ranges[562],scan->ranges[561],scan->ranges[560],scan->ranges[559],scan->ranges[157],scan->ranges[158],scan->ranges[159],scan->ranges[160]); 
 }
+  
+ 
+ 
+  static double avg1, avg2, avg3;
 
+};
+
+double ParkingRobot::avg1 = 0;
+double ParkingRobot::avg2 = 0;
+double ParkingRobot::avg3 = 0;
 
 int main(int argc, char **argv)
 {
   
   ros::init(argc, argv, "move_aitech_robot");
 
-  
-  
+  ParkingRobot R;
+
   ros::NodeHandle nh;
-
   ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Subscriber sub = nh.subscribe<sensor_msgs::LaserScan>("/aitech_robot/laser/scan",1000,ParkingRobot::Callback);
 
-  ros::Subscriber sub = nh.subscribe<sensor_msgs::LaserScan>("/aitech_robot/laser/scan",1000,Callback);
-
-  geometry_msgs::Twist msg;
 
   ros::Rate loop_rate(10);
 
   while (ros::ok())
-  {
+  { 
 
-    //std::cout<<range2point(0.1,0.1,0.1,0.2,1).back()<<std::endl;
+    geometry_msgs::Twist msg;
+    const double FORWARD_SPEED_MPS = 0.5;
+
+    msg.linear.x = FORWARD_SPEED_MPS;
+
     
+    if(ParkingRobot::avg1 > 0 && ParkingRobot::avg3 == 0 ){
+      
+        msg.linear.x = 0;
+      }
+
     pub.publish(msg);
- 
+
     ros::spinOnce();
 
     loop_rate.sleep();
-    
+  
   }
+  
 
 
   return 0;
